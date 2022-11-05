66c37dad88b915ad1353009a515d94cca6b74638
yopaseopor <yopaseopor@gmail.com>
2022-11-06 00:15:03 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -938,9 +938,9 @@ style: function (feature) {
 				},
 		{
 			group: 'Incline',
-			title: 'Exact Incline Moderate Down',
-			query: '(nwr["highway"!="steps"]["incline"~"^-[0-9]%"]({{bbox}});node(w););out meta;',
-			iconSrc: imgSrc + 'icones/accessibilitat/incline_down_5.svg',
+			title: 'Exact Incline Nul',
+			query: '(nwr["highway"!="steps"]["incline"~"0.[0-9].%"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/accessibilitat/maxspeed_empty.svg',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
 style: function (feature) {
 				var key_regex = /^incline$/
@@ -955,7 +955,43 @@ style: function (feature) {
 				});
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
-							src: imgSrc + 'icones/accessibilitat/incline_down_5.svg',
+							src: imgSrc + 'icones/maxspeed_empty.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
+			group: 'Incline',
+			title: 'Exact Incline Moderate Down',
+			query: '(nwr["highway"!="steps"]["incline"~"^-[0-9]%"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'accessibilitat/incline_down_5.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^incline$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'accessibilitat/incline_down_5.svg',
 							scale:0.03
 						}),
 							text: new ol.style.Text({
