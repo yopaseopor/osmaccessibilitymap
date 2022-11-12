0507a65d8cc78cbe733759edf16c89bfd6f4b45f
yopaseopor <yopaseopor@gmail.com>
2022-11-12 10:52:36 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1260,6 +1260,42 @@ style: function (feature) {
 				return style;
 			}
 				},
+		{
+			group: 'Incline',
+			title: 'Incline Unknown',
+			query: '(nwr["highway"!="steps"]["incline"!]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'accessibilitat/incline_question.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^incline$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'accessibilitat/incline_question.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
 
 		{
 			group: 'Vorades',
